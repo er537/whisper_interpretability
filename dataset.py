7da5923e993822386ca58ac55cec6893788db59d
Ellena Reid <ellenar@speechmatics.com>
2023-06-23 17:04:18 +0100

    add ClassWise dataloader

--- dataset.py
+++ dataset.py
@@ -1,9 +1,10 @@
 import torchaudio
-import whisper
 import torch
 import os
+import numpy as np
+from collections import defaultdict
 
-from utils import device
+from utils import device, load_audio, trim_audio
 
 class LibriSpeech(torch.utils.data.Dataset):
     """
@@ -24,3 +25,43 @@ class LibriSpeech(torch.utils.data.Dataset):
     def __getitem__(self, item):
         audio, sample_rate, text, _, _, _ = self.dataset[item]
         return audio, sample_rate, text, audio.shape[1]
+
+
+class ClasswiseDataset(torch.utils.data.IterableDataset):
+    def __init__(self, audio_samples_per_class=1000, dblx_path='/data/artefacts/vad/top_4_no_header/filtered/filtered_train.dblx', batch_size=10):
+        super().__init__()
+        self.build_dataset(dblx_path, audio_samples_per_class)
+        self.data = defaultdict(list)
+        self.build_dataset()
+        self.max_frames_per_input = 48_0000
+        self.audio_samples_per_class = audio_samples_per_class
+        self.batch_size = batch_size
+        
+    def build_dataset(self, dblx_path, audio_samples_per_class):
+        """
+        Fill self.samples with audio_samples_per_class
+        """
+        # TODO: this is kinda inefficient as we parse every audio file even if that class if already 'full'
+        with open(dblx_path, 'r') as f:
+            while not completed:
+                line = f.readline()
+                _, audio_path, start_time, end_time, label = line.split(' ')
+                if self.data[label].shape[0] > audio_samples_per_class:
+                    continue
+                audio = load_audio(audio_path)
+                trimmed_audio = trim_audio(audio, float(start_time), float(end_time))
+                self.data[label] = np.concatenate((self.data[label], trimmed_audio), axis=0)
+                completed = all(frames.shape[0] > audio_samples_per_class for frames in self.data.keys())
+    
+    def __iter__(self):
+        # TODO: drop last?
+        for label, samples in self.data.keys():
+            samples_yielded = 0
+            while samples_yielded < self.audio_samples_per_class:
+                audio_batch = []
+                for _ in range(self.batch_size):
+                    audio_batch.append(samples[samples_yielded:samples_yielded+self.audio_samples_per_class])
+                    samples_yielded += self.audio_samples_per_class
+                
+                yield np.stack(audio_batch, dim=0), label.repeat(self.batch_size)
+        
--- utils.py
+++ utils.py
@@ -1,3 +1,64 @@
 import torch
+from subprocess import CalledProcessError, run
+import numpy as np
+import torch.nn.functional as F
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def trim_audio(
+    array: np.array,
+    start_time: float,
+    end_time: float,
+    sample_rate: int = 16_000,
+):
+    """
+    Trim the audio file base array to n_samples, as expected by the encoder.
+    """
+    start_frame = int(sample_rate * start_time)
+    end_frame = int(sample_rate * end_time)
+    assert len(array) > end_frame - start_frame
+
+    return array[start_frame:end_frame]
+
+
+def load_audio(file: str, sample_rate_hz: int = 16_000):
+    """
+    Taken from Whisper repo: https://github.com/openai/whisper/blob/main/whisper/audio.py
+
+    Open an audio file and read as mono waveform, resampling as necessary
+
+    Parameters
+    ----------
+    file: str
+        The audio file to open
+
+    sample_rate_hz: int
+        The sample rate to resample the audio if necessary
+
+    Returns
+    -------
+    A NumPy array containing the audio waveform, in float32 dtype.
+    """
+
+    # This launches a subprocess to decode audio while down-mixing
+    # and resampling as necessary.  Requires the ffmpeg CLI in PATH.
+    # fmt: off
+    cmd = [
+        "ffmpeg",
+        "-nostdin",
+        "-threads", "0",
+        "-i", file,
+        "-f", "s16le",
+        "-ac", "1",
+        "-acodec", "pcm_s16le",
+        "-ar", str(sample_rate_hz),
+        "-"
+    ]
+    # fmt: on
+    try:
+        out = run(cmd, capture_output=True, check=True).stdout
+    except CalledProcessError as e:
+        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+
+    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
