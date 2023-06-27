4389bdea569fd959a0e9ed80daec3d4e0c33f63f
Ellena Reid <ellenar@speechmatics.com>
2023-06-27 16:01:04 +0100

    yield VAD data one sample at a time

--- probes/dataset.py
+++ probes/dataset.py
@@ -9,7 +9,7 @@ from utils import device, load_audio, trim_audio
 class VADDataset(torch.utils.data.IterableDataset):
     def __init__(
         self,
-        dblx_path="/data/artefacts/vad/top_4_no_header/filtered/filtered_train.dblx",
+        dblx_path="/home/ellenar/testing_vad.dblx",
         class_labels=["SPEECH", "NON_SPEECH"],
         pad=True,  ## pad/trim for Whisper
         max_audio_samples=math.inf,
@@ -18,7 +18,7 @@ class VADDataset(torch.utils.data.IterableDataset):
     ):
         super().__init__()
         self.dblx = open(dblx_path, "r")
-        self.datasets = {class_label: torch.empty(0) for class_label in class_labels}
+        self.datasets = {class_label: [] for class_label in class_labels}
         self.samples_per_batch = 48_0000  # default window size used by Whisper
         self.pad = pad
         self.max_audio_samples = max_audio_samples
@@ -34,7 +34,7 @@ class VADDataset(torch.utils.data.IterableDataset):
 
     def __iter__(self):
         """
-        Keep a rolling buffer (self.datasets) of audio samples for each class, yield once we have enough samples
+        Keep a rolling buffer (self.datasets) of audio samples for each class, yield once we have a sample of the required class
         """
         chosen_dataclass = random.sample(self.class_labels, 1)[0]
         while True:
@@ -51,42 +51,27 @@ class VADDataset(torch.utils.data.IterableDataset):
             trimmed_audio = torch.tensor(
                 trim_audio(audio, float(start_time), float(end_time))
             )
-            if self.datasets[label].shape[0] == 0:
-                self.datasets[label] = trimmed_audio
-            else:
-                self.datasets[label] = torch.cat(
-                    (self.datasets[label], trimmed_audio), dim=0
-                )
+            self.datasets[label].append(trimmed_audio)
 
             if label == chosen_dataclass:
-                if self.datasets[label].shape[0] > self.samples_per_batch:
-                    audio = self.datasets[label][: self.samples_per_batch]
-                    self.datasets[label] = self.datasets[label][
-                        self.samples_per_batch :
-                    ]  # discard used frames
-                    mels = self._get_mels(audio)
-                    print(mels.shape)
-                    labels = torch.tensor(self.class_labels.index(label)).repeat(
-                        mels.shape[0]
-                    )
-                    yield mels, labels
-                    chosen_dataclass = random.sample(self.class_labels, 1)[
-                        0
-                    ]  # new class
+                audio = self.datasets[label].pop(0)
+                mels = self._get_mels(audio)
+                labels = torch.tensor(self.class_labels.index(label)).repeat(
+                    mels.shape[0]
+                )
+                yield mels, labels
+                chosen_dataclass = random.sample(self.class_labels, 1)[0]  # new class
 
         # exhaust remaining buffers
         incomplete = {label: True for label in self.class_labels}
         while any(incomplete.values()):
             for label in self.class_labels:
-                if self.datasets[label].shape[0] > self.samples_per_batch:
-                    audio = self.datasets[label][: self.samples_per_batch]
+                if len(self.datasets[label]) > 0:
+                    audio = self.datasets[label].pop(0)
                     mels = self._get_mels(audio)
-                    labels = torch.tensor(self.class_labels.index[label]).repeat(
+                    labels = torch.tensor(self.class_labels.index(label)).repeat(
                         mels.shape[0]
                     )
                     yield mels, labels
-                    self.datasets[label] = self.datasets[label][
-                        self.samples_per_batch :
-                    ]
                 else:
                     incomplete[label] = False
