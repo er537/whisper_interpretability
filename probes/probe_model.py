ada9dd8533320d33cb8612cca0fb32f227913ee4
er537 <elliereid98@outlook.com>
2023-06-29 19:03:45 +0100

    make probe use all seq_positions

--- probes/probe_model.py
+++ probes/probe_model.py
@@ -5,8 +5,10 @@ class Probe(nn.Module):
     def __init__(self, feat_dim, outdim=2) -> None:
         super().__init__()
         self.w1 = nn.Linear(feat_dim, outdim, bias=False)
+        self.w2 = nn.Linear(1500, 1, bias=False)
         self.softmax = nn.Softmax(dim=2)
 
     def forward(self, x):
         x = self.w1(x)
-        return self.softmax(x)
+        x = self.w2(x.permute(0, 2, 1)) # bsz, outdim, seq_len
+        return self.softmax(x.permute(0, 2, 1))
